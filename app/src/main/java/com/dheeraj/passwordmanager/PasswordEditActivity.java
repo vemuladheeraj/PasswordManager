348eedcbc2accc45b82b4c4c18c69febc30139dd
vemula dheeraj <vemula.dheeraj@gmail.com>
2022-06-25 22:35:47 +0530

    version 2 changes

---
+++ app/src/main/java/com/dheeraj/passwordmanager/PasswordEditActivity.java
@@ -0,0 +1,14 @@
+package com.dheeraj.passwordmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_edit);
+    }
+}
